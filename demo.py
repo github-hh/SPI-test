39fa08395de0cea8fef3287b885545ed7b636944
github-hh <heon.huh@gmail.com>
2017-04-24 23:06:19 +0900

    Add files via upload

--- demo.py
+++ demo.py
@@ -2,6 +2,11 @@ import cv2
 
 image = cv2.imread("Tulips.jpg")
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+# plt.imshow(mpimg.imread('Tulips.png'))
+
+
 cv2.imshow("Over the Clouds", image)
 cv2.imshow("Over the Clouds - gray", gray_image)
 cv2.waitKey(0)
@@ -9,11 +14,21 @@ cv2.destroyAllWindows()
 
 si=gray_image.shape
 
-for ii in range(si[0]-1):
-    for jj in range(si[1]-1):
-        gray_image[ii,jj]=int(ii*jj/si[0]/si[1]*256)
+for ii in range(si[0]):
+    for jj in range(si[1]):
+        gray_image[ii,jj]=ii*jj/si[0]/si[1]*256
 
 
 cv2.imshow("test", gray_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+# plt.colormaps()
+# plt.imshow(gray_image,cmap='gray')
+plt.imshow(gray_image, cmap=cm.gray)
+plt.show()
+
+from numpy import *
+a=array([[2,3,4],[1,2,3]])
+c=array([1,2, 3, 4, 5, 6, 7, 8])
+c.reshape(-1,2)
+
